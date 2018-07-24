5244fe70586fb4b2fdceabc1a1c8e535e1d5e95d
harper <haibo1047@gmail.com>
2018-07-24 23:35:39 +0800

    re-construct model

---
+++ src/main/java/com/ylsq/frame/base/dao/model/SecuUser.java
@@ -0,0 +1,195 @@
+package com.ylsq.frame.base.dao.model;
+
+import java.io.Serializable;
+
+import com.ylsq.common.base.BaseModel;
+
+public class SecuUser extends BaseModel implements Serializable {
+    /**
+     * 编号
+     *
+     * @mbg.generated
+     */
+    private Long userId;
+
+    /**
+     * 帐号
+     *
+     * @mbg.generated
+     */
+    private String username;
+
+    /**
+     * 密码MD5(密码+盐)
+     *
+     * @mbg.generated
+     */
+    private String password;
+
+    /**
+     * 盐
+     *
+     * @mbg.generated
+     */
+    private String salt;
+
+    /**
+     * 姓名
+     *
+     * @mbg.generated
+     */
+    private String realname;
+
+    /**
+     * 电话
+     *
+     * @mbg.generated
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     *
+     * @mbg.generated
+     */
+    private String email;
+
+    /**
+     * 性别
+     *
+     * @mbg.generated
+     */
+    private String gender;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", userId=").append(userId);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", salt=").append(salt);
+        sb.append(", realname=").append(realname);
+        sb.append(", phone=").append(phone);
+        sb.append(", email=").append(email);
+        sb.append(", gender=").append(gender);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", createUser=").append(createUser);
+        sb.append(", recStatus=").append(recStatus);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        SecuUser other = (SecuUser) that;
+        return (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
+            && (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+            && (this.getSalt() == null ? other.getSalt() == null : this.getSalt().equals(other.getSalt()))
+            && (this.getRealname() == null ? other.getRealname() == null : this.getRealname().equals(other.getRealname()))
+            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()))
+            && (this.getEmail() == null ? other.getEmail() == null : this.getEmail().equals(other.getEmail()))
+            && (this.getGender() == null ? other.getGender() == null : this.getGender().equals(other.getGender()))
+            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
+            && (this.getCreateUser() == null ? other.getCreateUser() == null : this.getCreateUser().equals(other.getCreateUser()))
+            && (this.getRecStatus() == null ? other.getRecStatus() == null : this.getRecStatus().equals(other.getRecStatus()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getUserId() == null) ? 0 : getUserId().hashCode());
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getSalt() == null) ? 0 : getSalt().hashCode());
+        result = prime * result + ((getRealname() == null) ? 0 : getRealname().hashCode());
+        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
+        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
+        result = prime * result + ((getGender() == null) ? 0 : getGender().hashCode());
+        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        result = prime * result + ((getCreateUser() == null) ? 0 : getCreateUser().hashCode());
+        result = prime * result + ((getRecStatus() == null) ? 0 : getRecStatus().hashCode());
+        return result;
+    }
+}
