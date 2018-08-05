21e9205f87832596da6b9274020c6f85f44d4017
harper <haibo1047@gmail.com>
2018-08-05 22:33:17 +0800

    search by role name

--- src/main/java/com/ylsq/frame/base/service/SecuRoleService.java
+++ src/main/java/com/ylsq/frame/base/service/SecuRoleService.java
@@ -6,8 +6,14 @@ import com.ylsq.frame.base.dao.model.SecuRoleExample;
 
 /**
 * SecuRoleService接口
-* Created by shuzheng on 2018/8/2.
+* Created by shuzheng on 2018/8/5.
 */
 public interface SecuRoleService extends BaseService<SecuRole, SecuRoleExample> {
 
+	/**
+	 * 根据roleName查询角色
+	 * @param roleName
+	 * @return
+	 */
+	public SecuRole selectByRoleName(String roleName);
 }
--- src/main/java/com/ylsq/frame/base/service/impl/SecuRoleServiceImpl.java
+++ src/main/java/com/ylsq/frame/base/service/impl/SecuRoleServiceImpl.java
@@ -1,20 +1,23 @@
 package com.ylsq.frame.base.service.impl;
 
-import com.ylsq.common.annotation.BaseService;
-import com.ylsq.common.base.BaseServiceImpl;
-import com.ylsq.frame.base.dao.mapper.SecuRoleMapper;
-import com.ylsq.frame.base.dao.model.SecuRole;
-import com.ylsq.frame.base.dao.model.SecuRoleExample;
-import com.ylsq.frame.base.service.SecuRoleService;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ylsq.common.annotation.BaseService;
+import com.ylsq.common.base.BaseServiceImpl;
+import com.ylsq.frame.base.dao.mapper.SecuRoleMapper;
+import com.ylsq.frame.base.dao.model.SecuRole;
+import com.ylsq.frame.base.dao.model.SecuRoleExample;
+import com.ylsq.frame.base.service.SecuRoleService;
+
 /**
 * SecuRoleService实现
-* Created by shuzheng on 2018/8/2.
+* Created by shuzheng on 2018/8/5.
 */
 @Service
 @Transactional
@@ -25,5 +28,19 @@ public class SecuRoleServiceImpl extends BaseServiceImpl<SecuRoleMapper, SecuRol
 
     @Autowired
     SecuRoleMapper secuRoleMapper;
+
+	@Override
+	public SecuRole selectByRoleName(String roleName) {
+		// TODO Auto-generated method stub
+		SecuRoleExample example = new SecuRoleExample();
+		example.createCriteria().andRoleNameEqualTo(roleName);
+		List<SecuRole> roleList = selectByExample(example);
+		for(SecuRole role : roleList) {
+			if(role.getRecStatus() == SecuRole.recStatus_valid)
+				return role;
+			log.debug(role.getRoleName() + "is not available");
+		}
+		return null;
+	}
 
 }
