644822ea0a6ef7879e5fd03aba7af6bb450b4d1a
harper <haibo1047@gmail.com>
2018-08-14 23:23:19 +0800

    菜单排序，界面整理

--- src/main/java/com/ylsq/common/base/SystemConstants.java
+++ src/main/java/com/ylsq/common/base/SystemConstants.java
@@ -3,4 +3,5 @@ package com.ylsq.common.base;
 public class SystemConstants {
 
 	public static String System_Menu_Key = "mymenus_session_id";
+	public static String System_Login = "system_login_name_";
 }
--- src/main/java/com/ylsq/common/secu/shiro/realm/SecuRealm.java
+++ src/main/java/com/ylsq/common/secu/shiro/realm/SecuRealm.java
@@ -1,6 +1,7 @@
 package com.ylsq.common.secu.shiro.realm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -18,12 +19,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ylsq.common.base.SysParamEnum;
 import com.ylsq.common.base.SystemConstants;
 import com.ylsq.frame.base.dao.model.SecuMenu;
 import com.ylsq.frame.base.dao.model.SecuMenuExample;
 import com.ylsq.frame.base.dao.model.SecuUser;
+import com.ylsq.frame.base.obj.MenuObject;
 import com.ylsq.frame.base.service.SecuMenuService;
 import com.ylsq.frame.base.service.SecuUserService;
+import com.ylsq.frame.base.sorter.MenuSorter;
+import com.ylsq.frame.sys.dao.model.SysParamValue;
+import com.ylsq.frame.sys.service.SysParamValueService;
 
 public class SecuRealm extends AuthenticatingRealm {
 	private static Logger log = LoggerFactory.getLogger(SecuRealm.class);
@@ -32,6 +38,8 @@ public class SecuRealm extends AuthenticatingRealm {
 	private SecuUserService secuUserService;
 	@Autowired
 	private SecuMenuService secuMenuService;
+	@Autowired
+	private SysParamValueService sysParamValueService;
 	
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
@@ -46,16 +54,29 @@ public class SecuRealm extends AuthenticatingRealm {
 			log.error("login failed");
 			throw new IncorrectCredentialsException();
 		}
+		List<SysParamValue> moduleList = sysParamValueService.selectByParamName(SysParamEnum.Menu_Module.getConstant());
+		Map<String,Integer> moduleMap = new HashMap<>();
+		for(SysParamValue m : moduleList)
+			moduleMap.put(m.getValue1(), Integer.parseInt(m.getValue2()));
 		List<SecuMenu> menuList = secuMenuService.selectByExample(new SecuMenuExample());
-		Map<String,List<SecuMenu>> menuMap = new LinkedHashMap<>();
-		for(SecuMenu sm : menuList) {
-			List<SecuMenu> subList = menuMap.get(sm.getMenuModule());
+		List<MenuObject> mList = new ArrayList<>(menuList.size());
+		for(SecuMenu m : menuList) {
+			MenuObject sm = new MenuObject(m);
+			sm.setModuleOrder(moduleMap.get(m.getMenuModule()));
+			mList.add(sm);
+		}
+		Collections.sort(mList, new MenuSorter());
+		Map<String,List<MenuObject>> menuMap = new LinkedHashMap<>();
+		for(MenuObject sm : mList) {
+			List<MenuObject> subList = menuMap.get(sm.getModule());
 			if(subList == null) {
-				menuMap.put(sm.getMenuModule(), new ArrayList<SecuMenu>());
+				menuMap.put(sm.getModule(), new ArrayList<MenuObject>());
 			}
-			menuMap.get(sm.getMenuModule()).add(sm);
+			menuMap.get(sm.getModule()).add(sm);
 		}
 		SecurityUtils.getSubject().getSession().setAttribute(SystemConstants.System_Menu_Key, menuMap);
+		SecurityUtils.getSubject().getSession().setAttribute(SystemConstants.System_Login, userName);
+		
 		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(userName, passwd, getName());
 		return info;
 	}
